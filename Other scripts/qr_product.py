b9b7f18e8fc41b254b112e1488fdf14fcf2edcc7
SISHRAK <barashko04@gmail.com>
2023-05-30 13:52:41 +0300

    PipiPupu

--- Other scripts/qr_product.py
+++ Other scripts/qr_product.py
@@ -1,12 +1,8 @@
 import cv2
 import pyzbar.pyzbar as pyzbar
 import numpy as np
-import openpyxl
-import pandas as pd
-
 def camera_work():
     print("OK")
-    barCode = -1
     font = cv2.FONT_HERSHEY_SIMPLEX
     cap = cv2.VideoCapture(0)
     while (cap.isOpened()):
@@ -25,26 +21,14 @@ def camera_work():
                 hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
                 hull = list(map(tuple, np.squeeze(hull)))
             else:
-                hull = points
+                hull = points;
 
             n = len(hull)
             for j in range(0, n):
                 cv2.line(frame, hull[j], hull[(j + 1) % n], (255, 0, 0), 3)
+
+            # Запись в бд и вывод в терминал
             barCode = int(decodedObject.data)
-
-            if barCode != -1:
-                print(barCode)
-                return barCode
-
-
+            print(barCode)
 if __name__ == '__main__':
-    barCode = camera_work()
-    prod = pd.read_excel('data.xlsx')
-    # print(prod)
-    # print(prod.head())
-    chosen = prod.loc[(prod['id'] == barCode)]
-    print(chosen)
-    already_cart = pd.read_excel('cart.xlsx')
-    already_cart = pd.concat([already_cart, chosen])
-    with pd.ExcelWriter('cart.xlsx') as writer:
-        already_cart.to_excel(writer, sheet_name='cartname')
+    camera_work()
--- web_app/lk/views.py
+++ web_app/lk/views.py
@@ -1,5 +1,58 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+import numpy as np
 
-
+from pyzbar.pyzbar import decode
+import pandas as pd
 def lk(request):
-    return render(request, 'lk/lk.html')
+     return render(request, 'lk/lk.html')
+import cv2
+def run_script(request):
+    # Ваш Python-скрипт
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, img = cap.read()
+        cv2.imshow("camera", img)
+        if cv2.waitKey(10) == 27: # Клавиша Esc
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+def from_bd(BarCode):
+    prod = pd.read_excel('../../Other scripts/pipip.xlsx')
+
+    chosen = prod.loc[(prod['A'] == BarCode)]
+    print(chosen)
+
+
+
+def camera_work(request):
+    print("OK")
+    barCode = -1
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cap = cv2.VideoCapture(0)
+    while (cap.isOpened()):
+        # чтение с камеры
+        ret, frame = cap.read()
+        im = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # декодинг
+        decodedObjects = pyzbar.decode(im)
+
+        for decodedObject in decodedObjects:
+            points = decodedObject.polygon
+
+            # выделяет сам qr код в квадрат(немного магии)
+            if len(points) > 4:
+                hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
+                hull = list(map(tuple, np.squeeze(hull)))
+            else:
+                hull = points
+
+            n = len(hull)
+            for j in range(0, n):
+                cv2.line(frame, hull[j], hull[(j + 1) % n], (255, 0, 0), 3)
+            barCode = int(decodedObject.data)
+
+            if barCode != -1:
+                print(barCode)
+            from_bd(BarCode=barCode)
